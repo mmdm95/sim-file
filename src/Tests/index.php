402d29e859a52c38de1b883347c858e189e716d5
MMDM <mmdehghan95@gmail.com>
2020-09-29 15:16:03 +0330

    regex filter tested

--- src/Tests/index.php
+++ src/Tests/index.php
@@ -6,7 +6,8 @@ $filename = dirname(__DIR__);
 
 $files = \Sim\File\FileSystem::getDirFilteredFiles($filename, [
     new \Sim\File\Filters\TypeFilter(\Sim\File\Interfaces\IFileSystem::TYPE_FILE),
-    new \Sim\File\Filters\SizeFilter('10kb', '3.5kb'),
+//    new \Sim\File\Filters\SizeFilter('10kb', '3.5kb'),
+    new \Sim\File\Filters\RegexFilter('/ad\.php$/'),
 ]);
 
 /**
